90f866561b13b79eddbc291bf4cb690d9185306d
Jason VenHuizen <jason@sharptree.io>
2024-12-20 18:40:05 -0800

    Bump versions and documentation

--- src/maximo/maximo-client.js
+++ src/maximo/maximo-client.js
@@ -33,8 +33,8 @@ export default class MaximoClient {
         // keep a reference to the config for later use.
         this.config = config;
 
-        this.requiredScriptVersion = '1.41.0';
-        this.currentScriptVersion = '1.41.0';
+        this.requiredScriptVersion = '1.42.0';
+        this.currentScriptVersion = '1.42.0';
 
         this.scriptEndpoint = 'mxscript';
 
