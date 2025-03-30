49470d7628c09568c876e08f2739ca3fb8ac52fe
Jason VenHuizen <jason@sharptree.io>
2025-03-30 11:19:58 -0700

    Change from nashorn to javascript.

--- src/maximo/maximo-client.js
+++ src/maximo/maximo-client.js
@@ -33,8 +33,8 @@ export default class MaximoClient {
         // keep a reference to the config for later use.
         this.config = config;
 
-        this.requiredScriptVersion = '1.46.0';
-        this.currentScriptVersion = '1.46.0';
+        this.requiredScriptVersion = '1.47.0';
+        this.currentScriptVersion = '1.47.0';
 
         this.scriptEndpoint = 'mxscript';
 
@@ -1749,6 +1749,7 @@ export default class MaximoClient {
                 description: description,
                 status: activeStatus,
                 version: this.currentScriptVersion,
+                scriptlanguage: 'javascript',
                 source: source,
             };
 
